e39c9e2da549f8419c71f18c0fcb8dcca9470b35
Pablo <115142181+Readypablo@users.noreply.github.com>
2023-03-01 15:43:47 +0500

    Add files via upload

---
+++ wsr/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<header>
+
+<div class="left-header">
+    <img src="img/logo.svg" alt="" class="logo-header" >    
+</div>
+
+
+<div class="center-header">
+    <div class="nav-header"> <a href="#" class="link-header">Программа</a></div>
+    <div class="nav-header"> <a href="#" class="link-header">Спикеры</a></div>
+    <div class="nav-header"> <a href="#" class="link-header">Трансляции</a></div>
+    <div class="nav-header"> <a href="#" class="link-header">Личный кабинет</a></div>
+</div>
+
+<div class="right-header">
+    <input type="button" value="Вход" class="btn-header">
+</div>
+
+</header>
+
+<main>
+
+
+<div class="baner">
+
+    <div class="info-baner">
+   
+           
+    <div class="big-text"><p>Digital<br>Langue</p></div>
+     
+    <div>
+       <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и<br> вэб-дизайне. Lorem Ipsum является стандартной "рыбой" <br>для текстов на латинице с начала XVI века. </p>
+    </div>
+         <input type="button" value="Принять участие" id="btn-baner">
+    </div>
+
+    <div class="img-baner">
+    
+       <img src="img/baner.jpg" id="baner-img">
+    </div>
+
+</div>
+
+<h1 class="zagolovok"> 3 года работы</h1>
+<div class="infograf">
+
+    <div class="block-info">
+    <h1 class="ifgr-abs">10503</h1>
+        <img src="img/poset.jpg" alt="" width="100%" height="100%">
+        <p>Наших поситителей</p>
+    </div>
+
+    <div class="block-info">
+    <h1 class="ifgr-abs">146</h1>
+        <img src="img/expert.jpg" alt="" width="100%" height="100%">
+        <p>Привлеченых экспертов</p>
+    </div>
+    <div class="block-info">
+    <h1 class="ifgr-abs">213</h1>
+        <img src="img/investor.jpg" alt="" width="100%" height="100%">
+        <p>Привлеченых инвестеров</p>
+    </div>
+
+</div>
+<h1>11</h1>
+<h1>55</h1><h1>11</h1><h1>55</h1><h1>55</h1><h1>55</h1>
+
+
+</main>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
