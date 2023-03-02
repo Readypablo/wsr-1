ecd442e303c0363d0c17b0c11fd4e9880f9d24ae
Pablo <115142181+Readypablo@users.noreply.github.com>
2023-03-02 09:26:23 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<header>
+
+<div class="left-header">
+    <img src="img/logo.svg" alt="" class="logo-header" >    
+</div>
+
+<!-- меню горизонтальное -->
+<div class="center-header">
+    <div class="nav-header"> <a href="#" class="link-header">Программа</a></div>
+    <div class="nav-header"> <a href="#" class="link-header">Спикеры</a></div>
+    <div class="nav-header"> <a href="#" class="link-header">Трансляции</a></div>
+    <div class="nav-header"> <a href="#" class="link-header">Личный кабинет</a></div>
+</div>
+<!-- меню горизонтальное -->
+
+<div class="right-header">
+    <input type="button" value="Вход" class="btn-header">
+</div>
+
+</header>
+
+<main>
+
+<!-- банер блок -->
+
+<div class="baner">
+
+    <div class="info-baner">
+   
+           
+    <div class="big-text"><p>Digital<br>Langue</p></div>
+     
+    <div>
+       <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и<br> вэб-дизайне. Lorem Ipsum является стандартной "рыбой" <br>для текстов на латинице с начала XVI века. </p>
+    </div>
+         <input type="button" value="Принять участие" id="btn-baner">
+    </div>
+
+    <div class="img-baner">
+    
+       <img src="img/baner.jpg" id="baner-img">
+    </div>
+
+</div>
+
+<h1 class="zagolovok"> 3 года работы</h1>
+
+<!-- инфограф блок -->
+<div class="infograf">
+
+    <div class="block-info">
+    <h1 class="ifgr-abs">10503</h1>
+        <img src="img/poset.jpg" alt="" width="100%" height="100%">
+        <p>Наших поситителей</p>
+    </div>
+
+    <div class="block-info">
+    <h1 class="ifgr-abs">146</h1>
+        <img src="img/expert.jpg" alt="" width="100%" height="100%">
+        <p>Привлеченых экспертов</p>
+    </div>
+    <div class="block-info">
+    <h1 class="ifgr-abs">213</h1>
+        <img src="img/investor.jpg" alt="" width="100%" height="100%">
+        <p>Привлеченых инвестеров</p>
+    </div>
+</div>
+
+
+<h1 class="zagolovok">Наши спикеры</h1>
+
+<div class="spiker-all">
+<div class="block-spiker">
+    
+<div>
+    <img src="img/1.jpg" width="100%" height="100%" class="spiker">
+    <p  class="txt-spiker">
+        <span>Леонид Парамонов</span> <br>
+        Директор коммерческой дирекции АО ГЛОНАСС</p>
+</div>
+
+<div>
+    <img src="img/2.jpg" width="100%" height="100%" class="spiker">
+    <p  class="txt-spiker">
+        <span>Роман Тарасенков</span> <br>Директор департамента технологической платформы
+Группы Эталон</p>
+</div>
+
+<div>
+    <img src="img/3.jpg" width="100%" height="100%" class="spiker">
+    <p  class="txt-spiker">
+    <span>Роман Тарасенков</span> <br>Директор департамента технологической платформы
+Группы Эталон</p>
+</div>
+
+<div>
+    <img src="img/4.jpg" width="100%" height="100%" class="spiker">
+    <p  class="txt-spiker">
+    <span>Роман Тарасенков</span> <br>Директор департамента технологической платформы
+Группы Эталон</p>
+</div>
+
+</div>
+
+<div class="block-spiker">
+        <div>
+    <img src="img/5.jpg" width="100%" height="100%" class="spiker">
+    <p  class="txt-spiker">
+    <span>Роман Тарасенков</span> <br>Директор департамента технологической платформы
+Группы Эталон</p>
+</div>
+<div>
+    <img src="img/6.jpg" width="100%" height="100%" class="spiker">
+    <p  class="txt-spiker">
+    <span>Роман Тарасенков</span> <br>Директор департамента технологической платформы
+Группы Эталон</p>
+</div>
+<div>
+    <img src="img/7.jpg" width="100%" height="100%" class="spiker">
+    <p  class="txt-spiker">
+    <span>Роман Тарасенков</span> <br>Директор департамента технологической платформы
+Группы Эталон</p>
+</div>
+<div>
+    <img src="img/8.jpg" width="100%" height="100%" class="spiker">
+    <p  class="txt-spiker">
+    <span>Роман Тарасенков</span> <br>Директор департамента технологической платформы
+Группы Эталон</p>
+        </div>
+</div>
+
+
+<input type="button" value="">
+
+
+
+</div>
+
+</main>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
